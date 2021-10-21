f03842e6ba57fe253e25ee103252b3126adbb3c6
Srinika Sharma <20wh1a6651@bvrithyderabad.edu.in>
2021-10-21 06:16:26 +0000

    Code Logic

---
+++ grid.py
@@ -0,0 +1,36 @@
+N = 4
+def printing(futo_arr):
+    for i in range(N):
+        for j in range(N):
+            print{futo_arr[i][j], end = " " )
+    print()
+
+def isSafe(grid, row, col, num):
+    for x in range(4):
+        if grid[row][x] == num:
+            return False
+    for x in range(4):
+        if grid[x][col] == num:
+            return False
+
+def solveFutoshiki(grid,row,col):
+    if(row == N-1 and col == N):
+        return True
+
+
+    if col == N:
+        row += 1
+        col = 0
+
+    if grid[row][col] > 0:
+        return solveFutoshiki(grid, row, col+1)
+    for num in range(1,N+1):
+        if isSafe(grid,row,col,num):
+            grid[row][col] = num
+            if solveFutoshiki(grid, row, col+1):
+                return True
+            grid[row][col] = 0
+        return False
+    
+    
+
