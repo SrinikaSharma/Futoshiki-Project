02de4e99f1aae327fab1f7810115d49f79d386b0
Srinika Sharma <20wh1a6651@bvrithyderabad.edu.in>
2021-10-22 14:16:37 +0000

    Code to take the input of the Futoshiki puzzle from the user.

--- FUTOSHIKI.py
+++ FUTOSHIKI.py
@@ -1,35 +1,14 @@
-# just an example to find the empty cells in the grid
-futoshiki = [[1,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
-def findCellNum(futoshiki):
-    for x in range(4):
-        for y in range(4):
-            if futoshiki[x][y] == 0:
-                print(x,y)
-    return
-print(findCellNum(futoshiki))
+List = []
+n = int(input("Size of puzzle NxN"))
+print("Enter Numbers")
+for i in range(n):
+    List1 = []
+    for j in range(n):
+        value = int(input("Value -- "))
+        List1.append(value)
+    List.append(List1)
 
-M = 4
-
-def isPresentRow(grid, row, num):
-	for x in range(4):
-		if grid[row][x] == num:
-			return False
-def isPresentCol(grid, col, num):		    
-	for x in range(4):
-		if grid[x][col] == num:
-			return False
-
-
-def Futoshiki(grid, row, col):
-
-	for x in range(4):
-                for y in range(4):
-                        if x < 4:
-                                x++
-                        if x = 4:
-                                x = 0
-                                y++
-        
+print(List)
                                 
 
     
