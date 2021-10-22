d60fa9b0d3e05cd7d4f68fb1ef47f38237692c81
Sathwika Gaddam <20wh1a0519@bvrithyderabad.edu.in>
2021-10-22 14:20:00 +0000

    defining a function to solve the puzzle, i.e. checking empty cells, moving to the next cell, assigning values to cells.

--- FUTOSHIKI.py
+++ FUTOSHIKI.py
@@ -9,6 +9,23 @@ for i in range(n):
     List.append(List1)
 
 print(List)
+def solveFutoshiki(List,row,col):
+    if(row == n-1 and col == n):
+        return True
+
+    if col == n:
+        row += 1
+        col = 0
+
+    if List[row][col] > 0:
+        return solveFutoshiki(List, row, col+1)
+    for num in range(1,n+1):
+        if isNumPresent(List,row,col,num):
+            List[row][col] = num
+            if solveFutoshiki(List, row, col+1):
+                return True
+        List[row][col] = 0
+    return False  
                                 
 
     
